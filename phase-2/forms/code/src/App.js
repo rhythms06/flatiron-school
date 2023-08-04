d6c8a4f9a9d8066b888edaa189f6b33ac026c50f
Sakib Rasul <work@sakibrasul.com>
2023-08-04 12:27:35 -0400

    Implement an array in state

--- phase-2/forms/code/src/App.js
+++ phase-2/forms/code/src/App.js
@@ -6,6 +6,9 @@ By Sakib Rasul
 Core Deliverables
 1. Refactor <Form> into a "controlled" form.
 2. Add "Your lucky number is X!" after the two fields, where X is dependent on the two fields.
+3. Handle form submissions by appending the new user to a local array.
+4. Render the array as a list of members in `Form`.
+5. (Bonus!) Render the list of members in `App` instead of `Form`.
 
 */
 
@@ -13,7 +16,7 @@ import Form from './Form';
 
 function App() {
   return (
-    <div>
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
       <header>
         <h1>Luck of the Screen</h1>
       </header>
--- phase-2/forms/code/src/Form.js
+++ phase-2/forms/code/src/Form.js
@@ -4,49 +4,73 @@ import { useState } from "react";
 // In other words, we want to lift the state of each input up to their parent form.
 export default function Form() {
     // Since we've only got two fields to control, let's use one state to manage each.
-    const [fullName, setFullName] = useState("");
-    const [favoriteMovie, setFavoriteMovie] = useState("");
+    const [name, setName] = useState("");
+    const [movie, setMovie] = useState("");
 
     // For completeness, here's an alternative implementation with just one state.
     // This approach might make sense for a form with more than a few fields.
     // Just remember, always pass setState a **brand new object** :)
-    // let newUser = {
-    //     fullName: "",
-    //     favoriteMovie: ""
-    // }
-    // const [ user, setUser ] = useState(newUser);
-    // const { fullName, favoriteMovie } = user;
+    // const [ user, setUser ] = useState({ name: "", movie: "" });
+    // const { name, movie } = user;
 
     // We don't need a third state for luckyNumber, because it's derivable from existing state.
     // Note: charCodeAt() isn't something you need to know right now, I just threw it in for fun.
     // Note: This assignment works in the alternative implementation, too, as long as we deconstruct `user`.
     const luckyNumber =
-        (fullName ? (fullName.charCodeAt(0) + (fullName.length * 2)) : 1) +
-        (favoriteMovie ? (favoriteMovie.charCodeAt(0) + (favoriteMovie.length * 3)) : 1);
+        (name ? (name.charCodeAt(0) + (name.length * 2)) : 1) +
+        (movie ? (movie.charCodeAt(0) + (movie.length * 3)) : 1);
+    
+    // We'll need a third state for our list of members.
+    const [members, setMembers] = useState([]);
 
-    // To handle form submissions, append to the body of this function.
-    const handleSubmit = event => event.preventDefault(); 
+    // When the form is submitted, we'll call `setMembers()` to add to our list of members.
+    const handleSubmit = event => {
+        event.preventDefault();
+        // To update state, we always pass a **brand new object** to the state setter.
+        //    In this case, that means we pass an array with existing members, **and**
+        //    our newest member. To get existing members, we use spread syntax.
+        setMembers([
+            ...members,
+            {
+                id: members.length,
+                name: name,
+                movie: movie
+            }
+        ])
+        // To reset the form, we can revert our states to their initial values.
+        setName("");
+        setMovie("");
+        // Uncomment for an alternative implementation with just one state.
+        // setMembers([
+        //     ...members,
+        //     {
+        //         id: members.length,
+        //         ...user
+        //     }
+        // ])
+        // setUser({ name: "", movie: "" });
+    }
 
     return (
         <form onSubmit={handleSubmit}>
-            {/* To control an input, pass it a state getter (fullName) and setter (setFullName) as props (value, onChange). */}
-            <label><strong>Full Name </strong>
-                <input value={fullName} onChange={event => setFullName(event.target.value)} />
+            {/* To control an input, pass it a state getter (name) and setter (setName) as props (value, onChange). */}
+            <label><strong>Name </strong>
+                <input value={name} onChange={event => setName(event.target.value)} />
             </label>
-            {/* Uncomment for an implementation of "Full Name" with just one state: */}
+            {/* Uncomment for an implementation of "Name" with just one state: */}
             {/* To control an input, pass it a state getter (user) and setter (setUser) as props (value, onChange). */}
-            {/* <label><strong>Full Name </strong>
-                <input value={fullName} onChange={event => setUser({ fullName: event.target.value, favoriteMovie: favoriteMovie })} />
+            {/* <label><strong>Name </strong>
+                <input value={name} onChange={event => setUser({ name: event.target.value, movie: movie })} />
             </label> */}
             <br/><br/>
-            {/* To control an input, pass it a state getter (favoriteMovie) and setter (setFavoriteMovie) as props (value, onChange). */}
-            <label><strong>Favorite Movie </strong>
-                <input  value={favoriteMovie} onChange={event => setFavoriteMovie(event.target.value)}/>
+            {/* To control an input, pass it a state getter (movie) and setter (setMovie) as props (value, onChange). */}
+            <label><strong>Movie </strong>
+                <input value={movie} onChange={event => setMovie(event.target.value)}/>
             </label>
-            {/* Uncomment for an implementation of "Favorite Movie" with just one state: */}
+            {/* Uncomment for an implementation of "Movie" with just one state: */}
             {/* To control an input, pass it a state getter (user) and setter (setUser) as props (value, onChange). */}
-            {/* <label><strong>Favorite Movie </strong>
-                <input  value={favoriteMovie} onChange={event => setUser({ favoriteMovie: event.target.value, fullName: fullName })}/>
+            {/* <label><strong>Movie </strong>
+                <input value={movie} onChange={event => setUser({ movie: event.target.value, name: name })}/>
             </label> */}
             <br/><br/>
             {/* Let's render the user's lucky number! */}
@@ -54,8 +78,16 @@ export default function Form() {
             {/*       As an exercise, consider how difficult it might be to implement luckyNumber without lifting state. */}
             <em>Your lucky number is</em> <strong>{luckyNumber}</strong>!
             <br/><br/>
-            {/* You should probably change `value` before/after changing `handleSubmit`. */}
-            <input type="submit" value="This button doesn't do anything."/>
+            <input type="submit" value="Sign Up"/>
+            {/* Let's render our list of members with `Array.map()`! */}
+            <h2>Members</h2>
+            { members.length > 0 ?
+                members.map(member =>
+                    <p key={member.id}>
+                        <code>{member.name}</code><br/><em>{member.movie}</em>
+                    </p>)
+              : <sub>Fill out the form to add your first member!</sub>
+            }
         </form>
     )
 }
