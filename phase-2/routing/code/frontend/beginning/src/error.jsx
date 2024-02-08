68e1df263475ec8348d719e8e6c52965dd799797
Sakib Rasul <work@sakibrasul.com>
2024-02-08 11:41:20 -0500

    Back-propagate `routing/.../error` and `.../nav`

--- phase-2/routing/code/frontend/beginning/src/error.jsx
+++ phase-2/routing/code/frontend/beginning/src/error.jsx
@@ -1,11 +1,14 @@
+import { useRouteError, Link } from "react-router-dom"
+
 // This component will render on errors.
 export default function Error() {
+    const error = useRouteError();
     return (
         <div className="m-5">
             <h1 className="text-xl">Yikes!</h1>
             <p className="text-xs mb-2">Something went wrong.</p>
-            <p className="mb-3"><code>101: Beep boop.</code></p>
-            <a>🔙 take me home</a>
+            <p className="mb-3"><code>{error.status}: {error.statusText}</code></p>
+            <Link to="/">🔙 take me home</Link>
             <footer className="mt-4 text-xs">&copy; 2023 Sakib Rasul</footer>
         </div>
     )
--- phase-2/routing/code/frontend/beginning/src/nav.jsx
+++ phase-2/routing/code/frontend/beginning/src/nav.jsx
@@ -1,11 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 // A <nav> that links out to /, /teas, and /about.
 export default function Nav() {
     return (
         <nav className="flex justify-between">
-            <a>Sakib's Tea House</a>
+            <NavLink to="/" className={({ isActive }) => `text-2xl hover:italic ${isActive && 'font-semibold'}`}>
+                Sakib's Tea House
+            </NavLink>
             <div className="flex gap-5 items-center">
-                <a>Our Teas</a>
-                <a>About</a>
+                <NavLink to="/teas" className={({ isActive }) => `text-2xl hover:italic ${isActive && 'font-semibold'}`}>
+                    Our Teas
+                </NavLink>
+                <NavLink to="/about" className={({ isActive }) => `text-2xl hover:italic ${isActive && 'font-semibold'}`}>
+                    About
+                </NavLink>
             </div>
         </nav>
     )
