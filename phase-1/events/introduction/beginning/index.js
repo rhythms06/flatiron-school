720c5abcfc4f06ffdec50995600bce9653972d65
Sakib Rasul <work@sakibrasul.com>
2024-03-13 15:32:28 -0400

    init `events/introduction/beginning` and `.../end`

---
+++ phase-1/events/introduction/beginning/index.js
@@ -0,0 +1,24 @@
+/*
+
+Phase 1 -> DOM Events
+by Sakib Rasul
+Updated March 13, 2024
+Created September 13, 2023
+
+Objectives
+1. Do something when an event fires on an element.
+2. Handle a form submission by reading its input.
+
+Takeaways
+- "click" and "submit" events
+- `event.preventDefault()`
+- `[form].[input].value`
+
+*/
+
+// ~ the "click" event
+
+// ~ the "submit" event
+
+// ~ challenges
+// 1. Add some non-click, non-submit event listener to the page. It can trigger whatever you wish!
