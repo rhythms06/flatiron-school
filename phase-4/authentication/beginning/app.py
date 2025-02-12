364b0b2bf9235e35333bda0d6019356545b4f4cd
Sakib Rasul <work@sakibrasul.com>
2025-02-12 13:48:23 -0500

    Add `IntegrityError`, `make_response`, and `flask_restful`

--- phase-4/authentication/beginning/app.py
+++ phase-4/authentication/beginning/app.py
@@ -3,28 +3,40 @@
 
 # Objectives
 # 1. Set up Flask, SQLAlchemy, SerializerMixin, and Alembic.
-# 2. Import `session` from flask to set and get cookies.
-# 3. Set up `python-dotenv` to read variables in `.env`.
-# 4. Set up `flask_cors` to allow requests from foreign domains.
-# 5. Set up `flask_bcrypt` to translate passwords to/from hashes.
-# 6. Create a `User` model with a `username`,
+# 2. Import `session` from `flask` to set and get cookies.
+# 3. Import `make_response` from `flask` to create responses.
+# 4. Import `Resource` and `Api` from `flask_restful` to
+#    enable class-based routes. 
+# 5. Set up `python-dotenv` to read variables in `.env`.
+# 6. Set up `flask_cors` to allow requests from foreign domains.
+# 7. Set up `flask_bcrypt` to translate passwords to/from hashes.
+# 8. Set up an instance of `Api`. 
+# 9. Create a `User` model with a `username`,
 #    `password_hash`, `password`, and `authenticate()`.
-# 7. Write a route for creating a new user.
-# 8. Create an account.
-# 9. Write a route for logging in an existing user.
-# 10. Log in, and inspect your client's cookies.
-# 11. Write a route for retrieving the current user's details.
-# 12. Get the current user's details.
-# 13. Write a route for logging out the current user.
+# 10. Write a route for creating a new user.
+# 11. Create an account.
+# 12. Write a route for logging in an existing user.
+# 13. Log in, and inspect your client's cookies.
+# 14. Write a route for retrieving the current user's details.
+# 15. Get the current user's details.
+# 16. Write a route for logging out the current user.
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy_serializer import SerializerMixin
 from flask_migrate import Migrate
 
 # We'll use `session` to set and read a user's **cookies**,
 # i.e. things we'd like to remember about a user's visit
 # to our website (e.g. their ID).
-from flask import Flask, jsonify, request, session
+# We can optionally use `make_response` to enable a more
+# flexible way of crafting return values.
+from flask import Flask, jsonify, request, session, make_response
+
+# Flask-RESTful is an extension for Flask that provides
+# an alternative way of building routes, via classes
+# `Resource` and `Api`.
+from flask_restful import Resource, Api
 
 # We'll use python-dotenv to load variables from `.env`... 
 from dotenv import load_dotenv
@@ -62,6 +74,10 @@ CORS(app)
 # which we'll use to securely store and verify user passwords.
 bcrypt = Bcrypt(app)
 
+# This line initializes an instance of Flask-RESTful's API,
+# which we'll use to connect routing methods to endpoints.
+api = Api(app)
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -94,21 +110,31 @@ def create_user():
         db.session.add(new_user)
         db.session.commit()
         return jsonify(new_user.to_dict()), 201
+        # ...or return make_response(jsonify(new_user.to_dict()), 201)
+    except IntegrityError:
+        return jsonify({ "Error": "Username not given or already registered." }, 400)
+        # ...or return make_response(jsonify({"Error": "..."}), 400)
     except Exception as error:
         return jsonify({ "Error": str(error) }), 400
-    
+        # ...or return make_response(jsonify({"Error": str(error)}), 400)
+
 @app.post("/login")
 def login():
+    # Get the user's proposed credentials.
     data = request.json
     user = User.query.where(User.username == data["username"]).first()
+    # Lookup those credentials in the database.
+    # Check if the row exists *AND* if the given password is correct.
     if user and user.authenticate(data["password"]):
         # We can "log in" the user by
         # saving their `id` into a session cookie.
         session["user_id"] = user.id
         return jsonify(user.to_dict()), 201
+        # ...or return make_response(jsonify(user.to_dict()), 201)
     else:
         # The response status code 401 is short for "unauthorized".
         return jsonify({ "Error": "Invalid username or password." }), 401
+        # ...or return make_response(jsonify({"Error": "..."}), 401)
     
 @app.get("/current-user")
 def get_current_user():
@@ -121,6 +147,7 @@ def get_current_user():
         # to read forbidden property, a user's plaintext password.
         # print(user.password)
         return jsonify(user.to_dict()), 200
+        # ...or return make_response(jsonify(user.to_dict()), 200)
     else:
         # The response status code 401 is short for "no content",
         # i.e. no one is logged in.
@@ -133,7 +160,57 @@ def logout():
     # i.e. no one is logged in.
     return jsonify({ }), 204
 
+
+# (Optional) Refactoring with Flask-RESTful
+# If we have a class inherit from `Resource`,
+# it can redefine instance methods
+# `get`, `post`, `patch`, and `delete`,
+# which we can later map to endpoints with `api`.
+class UserResource(Resource):
+    def post(self):
+        data = request.json
+        try:
+            new_user = User(username=data["username"])
+            new_user.password = data["password"]
+            db.session.add(new_user)
+            db.session.commit()
+            # Flask-RESTful automatically converts
+            # responses into JSON, so we don't need
+            # to write `jsonify(...)`!
+            return new_user.to_dict(), 201
+        except IntegrityError:
+            return { "Error": "Username not given or already registered!" }, 400
+        except Exception as error:
+            return { "Error": str(error) }, 400
+
+class SessionResource(Resource):
+    def post(self):
+        data = request.json
+        user = User.query.filter_by(username=data["username"]).first()
+        if user and user.authenticate(data["password"]):
+            session["user_id"] = user.id
+            return user.to_dict(), 201
+        else:
+            return { "Error": "Invalid username or password." }, 401
     
+    def get(self):
+        user_id = session.get("user_id")
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+        else:
+            return "", 204
+
+    def delete(self):
+        session.pop("user_id", None)
+        return "", 204
+
+# `api.add_resource(<Resource>, <endpoint>)`
+# maps the methods in <Resource> to <endpoint>.
+api.add_resource(UserResource, "/users")
+api.add_resource(SessionResource, "/session")
+
+
 # ~ Challenges
 # 1. Create a one-to-many relationship between User and a new model of your choice.
 # 2. Write a POST route that associates a new instance of your new model to the
